0ed99f03cd96ed998c1f6f6f9c9433ab7253727b
Samir GLAGLOU <samir.glaglou2025@campus-eni.fr>
2025-05-09 11:36:16 +0200

    projet individuel 09/05/25 11:36

--- script_profil.js
+++ script_profil.js
@@ -3,28 +3,24 @@ const nameUser = sessionStorage.getItem("nameConnected");
 const emailUser = sessionStorage.getItem("emailConnected");
 const pwdUser = sessionStorage.getItem("passwordConnect");
 
-console.log(emailUser); // Affiche le nom de l'utilisateur
-console.log(nameUser); // Affiche le nom de l'utilisateur
+console.log(emailUser);
+console.log(nameUser);
 
-// Vérifie que les deux informations existent avant de les afficher dans le formulaire
 if (nameUser && pwdUser) {
   const container = document.getElementById("userConnected__info_container"); // Conteneur où afficher les champs
 
-  // Création du champ de nom utilisateur
   const champName = document.createElement("input");
   champName.type = "text";
   champName.name = "nom_user";
   champName.value = nameUser;
   champName.placeholder = "Nom d'utilisateur";
 
-  // Création du champ e-mail
   const champEmail = document.createElement("input");
   champEmail.type = "email";
   champEmail.name = "email_user";
   champEmail.value = emailUser;
   champEmail.placeholder = "Email";
 
-  // Ajout des champs au conteneur
   container.appendChild(champName);
   container.appendChild(document.createElement("br"));
   container.appendChild(champEmail);
